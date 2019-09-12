a68d27f07f539434815098bc2dc9987f6acc60d0
JeongBin <kjh3095000@gmail.com>
2019-09-12 23:46:18 +0900

    (정빈) LongestBitonicPartSequence 완료

--- Baekjoon Online Judge/12단계_동적계획법 1/(정빈) LongestBitonicPartSequence.java
+++ Baekjoon Online Judge/12단계_동적계획법 1/(정빈) LongestBitonicPartSequence.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /*
 문제
 수열 S가 어떤 수 Sk를 기준으로 S1 < S2 < ... Sk-1 < Sk > Sk+1 > ... SN-1 > SN을 만족한다면, 
@@ -22,6 +20,7 @@ import java.util.Scanner;
 7
 
  */
+import java.util.Scanner;
 public class LongestBitonicPartSequence {
 	static int[] sequence;
 	static int[] up;
@@ -39,38 +38,33 @@ public class LongestBitonicPartSequence {
 		for (int i = 0; i < num; i++) {
 			sequence[i] = bf.nextInt();
 		}
-		
+
 		// 올라갈때
 		for (int i = 0; i < num; i++) {
 			up[i] = 1;
 			for (int j = 0; j < i; j++) {
-				if(sequence[j]<sequence[i] && up[i]<up[j]+1) {
+				if (sequence[j] < sequence[i] && up[i] < up[j] + 1) {
 					up[i] = up[j] + 1;
 				}
 			}
 		}
-		//내려갈때
-		for (int i = num-1; i > 0; i--) {
+		// 내려갈때
+		for (int i = num - 1; i >= 0; i--) {
 			down[i] = 1;
-			for (int j = num-1; j > i; j--) {
-				if(sequence[j]<sequence[i] && down[i]<down[j]+1) {
+			for (int j = num - 1; j > i; j--) {
+				if (sequence[j] < sequence[i] && down[i] < down[j] + 1) {
 					down[i] = down[j] + 1;
 				}
 			}
 		}
-		//확인
+
+		// 최대값 구하기
 		for (int i = 0; i < num; i++) {
-//			System.out.print(up[i]+" ");
-			System.out.print(down[i]+" ");
-			
-		}
-		//최대값 구하기
-		for (int i = 0; i < num; i++) {
-			if((up[i]+down[i])>answer) {
-				answer = up[i]+down[i];
+			if ((up[i] + down[i]) > answer) {
+				answer = up[i] + down[i];
 			}
 		}
-
+		// 중복된거 1 빼기
 		System.out.println(answer - 1);
 
 	}
