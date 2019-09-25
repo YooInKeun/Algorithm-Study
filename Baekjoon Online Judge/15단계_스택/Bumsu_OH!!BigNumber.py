f3bf501ef0d6deaae3ee35a5c25d5b7902f327e1
bumsu <bumsu0211@gmail.com>
2019-09-25 15:04:40 +0900

    범수 오큰수

---
+++ Baekjoon Online Judge/15단계_스택/Bumsu_OH!!BigNumber.py
@@ -0,0 +1,66 @@
+'''
+문제
+크기가 N인 수열 A = A1, A2, ..., AN이 있다. 수열의 각 원소 Ai에 대해서 오큰수 NGE(i)를 구하려고 한다. Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다. 그러한 수가 없는 경우에 오큰수는 -1이다.
+
+예를 들어, A = [3, 5, 2, 7]인 경우 NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1이다. A = [9, 5, 4, 8]인 경우에는 NGE(1) = -1, NGE(2) = 8, NGE(3) = 8, NGE(4) = -1이다.
+
+입력
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째에 수열 A의 원소 A1, A2, ..., AN (1 ≤ Ai ≤ 1,000,000)이 주어진다.
+
+출력
+총 N개의 수 NGE(1), NGE(2), ..., NGE(N)을 공백으로 구분해 출력한다.
+
+예제 입력 1 
+4
+3 5 2 7
+예제 출력 1 
+5 7 7 -1
+예제 입력 2 
+4
+9 5 4 8
+예제 출력 2 
+-1 8 8 -1
+'''
+
+class stack:
+    def __init__(self):
+        self.key = 0
+        self.arr = []
+    
+    def push(self, num):
+        self.arr.append(num)
+        self.key += 1
+    
+    def pop(self):
+        if self.arr:
+            self.key -= 1
+            value = self.arr[self.key]
+            del(self.arr[self.key])
+            return value
+        else:
+            return - 1
+
+    def size(self):
+        return self.key
+
+    def top(self):
+        if self.arr:
+            return self.arr[self.key - 1]
+        else:
+            return -1
+
+N = int(input())
+nums = list(map(int, input().split(' ')))
+stack_inst = stack()
+NGE = [-1 for _ in range(N)]
+
+for i in range(N):
+    last_size = stack_inst.size()
+    while stack_inst.size() > 0 and stack_inst.top()[0] < nums[i]:
+        NGE[(stack_inst.pop())[1]] = nums[i]
+    stack_inst.push((nums[i], i))
+
+for item in NGE:
+    print(item, end=" ")
+
+
