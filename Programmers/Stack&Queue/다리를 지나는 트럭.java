322718883b6b98b72db0c5f9966f9a0d42ed6838
Yoo In Keun <keun0390@naver.com>
2020-07-28 23:40:37 +0900

    feat: 다리를 지나는 차량 문제 해결

--- Programmers/Stack&Queue/다리를 지나는 트럭.java
+++ Programmers/Stack&Queue/다리를 지나는 트럭.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-                
         int truckSize = truck_weights.length;
         int truckCount = 0;
         
