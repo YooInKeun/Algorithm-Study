6ce7c2fbb324787d3065ecb0d1ef5f6b81f43660
Yoo In Keun <keun0390@naver.com>
2020-07-30 22:23:52 +0900

    feat: 124 나라의 숫자 시간초과(65점)

--- Programmers/Practice/124 나라의 숫자.java
+++ Programmers/Practice/124 나라의 숫자.java
@@ -1,27 +1,45 @@
 class Solution {
     public String solution(int n) {
         StringBuffer answer = new StringBuffer();
-        
+
         int divider = 1;
         while (n / divider != 0) {
             divider *= 3;
         }
         divider /= 3;
-        
-        
+
         if (n % divider == 0 && divider != 1) {
             divider /= 3;
         }
-        
-        
+
         while (divider > 0) {
             int num = (n / divider);
-            answer.append(Integer.toString(num));
+            int appendedNum = num;
+            if (num == 3) {
+                appendedNum = 4;
+            }
+            answer.append(appendedNum);
             n -= num * divider;
             divider /= 3;
         }
-        
-        
-        return answer.toString().replaceAll("3", "4");
+
+        while (answer.indexOf("0") > -1) {
+            int pos = answer.indexOf("10");
+            if (pos > -1) {
+                answer.replace(pos, pos + 2, "04");
+            }
+            
+            pos = answer.indexOf("20");
+            if (pos > -1) {
+                answer.replace(pos, pos + 2, "14");
+            }
+            
+            pos = answer.indexOf("40");
+            if (pos > -1) {
+                answer.replace(pos, pos + 2, "24");                
+            }
+        }
+            
+        return answer.toString();
     }
 }
