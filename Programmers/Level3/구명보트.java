33d554d741c5977cf311f4312866e8b0c2e30adf
Yoo In Keun <keun0390@naver.com>
2020-12-15 01:11:25 +0900

    feat: 구명보트 풀이완료

--- Programmers/Level3/구명보트.java
+++ Programmers/Level3/구명보트.java
@@ -1,19 +1,38 @@
 import java.util.*;
 
 class Solution {
+    
+    private static int BIG_NUM = 1000;
+    private static int MIN_NUM = 40;
+    
     public int solution(int[] people, int limit) {
-        int answer = 1;
+        int answer = 0;
         int currentLimit = limit;
         Arrays.sort(people);
         
-        for (int person : people) {
-            if (currentLimit >= person) {
-                currentLimit -= person;
-            } else {
-                currentLimit = limit;
-                currentLimit -= person;
-                answer++;
+        for (int i = people.length - 1; i >= 0; i--) {
+            if (people[i] >= BIG_NUM) {
+                continue;
             }
+            currentLimit -= people[i];
+            answer++;
+            
+            for (int j = 0; j < i; j++) {
+                if (currentLimit < MIN_NUM) {
+                    break;
+                }
+                
+                if (currentLimit >= people[j]) {
+                    currentLimit -= people[j];
+                    people[j] = BIG_NUM;
+                    
+                    if (currentLimit < MIN_NUM) {
+                        break;
+                    }
+                }
+            }
+            
+            currentLimit = limit;
         }
         
         return answer;
